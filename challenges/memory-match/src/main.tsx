73860d5f22f34eb669836d83fa906a0f39f53828
Pierre Ferreira <pierre.ferreira@cloudsmiths.co.za>
2024-10-27 23:09:53 +0200

    :zap:Setup for ReacUI

--- challenges/memory-match/src/main.tsx
+++ challenges/memory-match/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './ReactUI.tsx'
+import ReactUI from './ReactUI.tsx'
+import initGame from './initGames.ts'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('ui')!).render(
   <StrictMode>
-    <App />
+    <ReactUI />
   </StrictMode>,
 )
+
+initGame();
