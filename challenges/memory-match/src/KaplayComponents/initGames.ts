6441e50e2c217d17adb0a75a92471ac4c49d1758
Pierre Ferreira <pierre.ferreira@cloudsmiths.co.za>
2024-11-28 22:46:05 +0200

    :zap: Memory-match game config.

--- challenges/memory-match/src/KaplayComponents/initGames.ts
+++ challenges/memory-match/src/KaplayComponents/initGames.ts
@@ -36,14 +36,14 @@ export default function initGame() {
 		const images: string[] = [
 			"apple",
 			"pineapple",
-			// "bean",
-			// "palm_tree",
-			// "gigagantrum",
-			// "eben-etzebeth",
-			// "siya-kolisi",
-			// "dolphin",
-			// "bag",
-			// "bobo",
+			"bean",
+			"palm_tree",
+			"gigagantrum",
+			"eben-etzebeth",
+			"siya-kolisi",
+			"dolphin",
+			"bag",
+			"bobo",
 			// "michael_scott",
 			// "cloud",
 			// "coin",
@@ -67,14 +67,14 @@ export default function initGame() {
 		];
 
 		// Initiate game variables.
-		const cntDoomCounter: number = 2;
+		const cntDoomCounter: number = 10;
 		store.set(initDoomCounterAtom, cntDoomCounter);
 		store.set(cntDoomCounterAtom, cntDoomCounter);
 
 		const solvedPairsForWin: number = images.length;
 		store.set(solvedPairsForWinAtom, solvedPairsForWin);
 
-		const maxGameTimeSec: number = 10;
+		const maxGameTimeSec: number = 90;
 		const maxCardsInRow: number = 5;
 		const cardSize: Vec2 = k.vec2(110, 130);
 		const infoBoardPos: Vec2 = k.vec2(500, 65);
